78080d5ff259366023a6d5a7b4a49a2877b257a4
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-10-01 19:44:18 +0100

    #11 connect view recipe to api

--- src/components/ViewRecipe/ViewRecipe.js
+++ src/components/ViewRecipe/ViewRecipe.js
@@ -1,22 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RecipeImage } from "./RecipeImage/RecipeImage";
 import { useLocation } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { Divider } from "@mui/material";
 
 export const ViewRecipe = () => {
   const location = useLocation();
   const recipeID = location.state.recipeID;
+  const [recipe, setRecipe] = useState({});
 
-  console.log(recipeID);
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + "/recipes/recipe/" + recipeID)
+      .then((response) => response.json())
+      .then((data) => setRecipe(data));
+  }, []);
 
-  // temps
-  const recipeName = "Ravioli";
+  console.log(recipe);
+
+  const recipeName = recipe.recipeName;
+  const recipeDescription = recipe.recipeDecsription;
   const imageSource =
     "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Flickr_-_cyclonebill_-_Ravioli_med_skinke_og_asparges_i_mascarponecreme.jpg/1200px-Flickr_-_cyclonebill_-_Ravioli_med_skinke_og_asparges_i_mascarponecreme.jpg";
 
-  const [recipe, setRecipe] = useState({});
   return (
     <>
       <RecipeImage imageSource={imageSource} recipeName={recipeName} />
+      <Box sx={{ paddingX: 1 }}>
+        <Box sx={{ paddingBottom: 3 }}>
+          <Typography variant="h2">{recipeName}</Typography>
+          <Typography variant="body1">{recipeDescription}</Typography>
+        </Box>
+        <Divider />
+        <Box sx={{ paddingBottom: 3 }}>
+          <Typography variant="body1">
+            the list of instructions will go here
+          </Typography>
+        </Box>
+      </Box>
     </>
   );
 };
