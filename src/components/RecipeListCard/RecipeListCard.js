36315feda72c2dc3c44f6878ccfe81fb31bb1f4b
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-12-26 11:40:59 +0000

    #8 passing correct recipe id on click

--- src/components/RecipeListCard/RecipeListCard.js
+++ src/components/RecipeListCard/RecipeListCard.js
@@ -13,7 +13,7 @@ export const RecipeListCard = (props) => {
   const navigate = useNavigate();
 
   const itemClickedOn = useCallback(
-    () => navigate("/ViewRecipe", { state: { recipeID: "1" } }),
+    () => navigate("/ViewRecipe", { state: { recipeID: id } }),
     [navigate]
   );
 
