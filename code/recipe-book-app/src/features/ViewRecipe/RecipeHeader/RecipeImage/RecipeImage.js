b92c67259b322c035084cd46676fe42ff5b5b7e5
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2023-04-27 06:44:44 +0100

    image adjustment

--- code/recipe-book-app/src/features/ViewRecipe/RecipeHeader/RecipeImage/RecipeImage.js
+++ code/recipe-book-app/src/features/ViewRecipe/RecipeHeader/RecipeImage/RecipeImage.js
@@ -3,13 +3,31 @@ import { Box } from "@mui/material";
 import PropTypes from "prop-types";
 
 export const RecipeImage = (props) => {
-	const imageSource = props.imageSource;
+	const source = props.imageSource;
 	const recipeName = props.recipeName;
-	return (
-		<Box sx={{ backgroundColor: "background.paper", maxWidth: "100%" }}>
-			<img className={css.RecipeImage} alt={recipeName} src={imageSource} />
-		</Box>
-	);
+	if (source) {
+		return (
+			<Box
+				sx={{
+					backgroundColor: "background.paper",
+					maxWidth: "100%",
+					minHeight: "15em",
+				}}
+			>
+				<img className={css.RecipeImage} alt={recipeName} src={source} />
+			</Box>
+		);
+	} else {
+		return (
+			<Box
+				sx={{
+					backgroundColor: "background.paper",
+					maxWidth: "100%",
+					height: "15em",
+				}}
+			></Box>
+		);
+	}
 };
 
 RecipeImage.propTypes = {
