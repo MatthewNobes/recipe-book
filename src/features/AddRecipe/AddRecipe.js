812810d836cfb91f275924a2650ceb2112d231e8
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2023-04-30 13:29:41 +0100

    #35 adding login redirect for adding recipes

--- src/features/AddRecipe/AddRecipe.js
+++ src/features/AddRecipe/AddRecipe.js
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
 import { RecipeForm, SubPageHeader } from "../../components";
+import supabase from "../../data/supabase";
+import { useNavigate } from "react-router-dom";
 
 export const AddRecipe = () => {
-	return (
-		<div>
-			<SubPageHeader headerText="Add Recipe" />
-			<RecipeForm />
-		</div>
-	);
+	const navigate = useNavigate();
+
+	const redirectToLogin = () => navigate("/Login");
+
+	const isAuthorised = supabase.realtime.accessToken ? true : false;
+
+	if (isAuthorised) {
+		return (
+			<>
+				<SubPageHeader headerText="Add Recipe" />
+				<RecipeForm />
+			</>
+		);
+	} else {
+		useEffect(() => {
+			redirectToLogin();
+		}, []);
+	}
 };
