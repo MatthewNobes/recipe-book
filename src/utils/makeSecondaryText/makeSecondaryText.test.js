b30f8007bf32e508b92212aee4dfa3dac0b08467
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2023-06-01 19:58:41 +0100

    #88 extending out description text

--- src/utils/makeSecondaryText/makeSecondaryText.test.js
+++ src/utils/makeSecondaryText/makeSecondaryText.test.js
@@ -1,13 +1,13 @@
 import { makeSecondaryText } from "./makeSecondaryText";
 
 describe("makeSecondaryText", () => {
-	it("should return the text cut down to 27 characters with ... on the end", () => {
+	it("should return the text cut down to 70 characters with ... on the end", () => {
 		const description =
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
 		const cutDownDescription = makeSecondaryText(description);
-		expect(cutDownDescription.length).toBe(30);
-		expect(cutDownDescription).toBe(description.slice(0, 27) + "...");
+		expect(cutDownDescription.length).toBe(73);
+		expect(cutDownDescription).toBe(description.slice(0, 70) + "...");
 		expect(
 			cutDownDescription.charAt(cutDownDescription.length - 3) +
 				cutDownDescription.charAt(cutDownDescription.length - 2) +
